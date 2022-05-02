e20c8dde10a889b474d904d4908addb86b224ac5
Dhwani <dev1@a-enigma.com>
2022-05-02 21:07:47 +0530

    initial commit

--- SpringCloudExample/src/main/java/com/example/SpringCloudExample/SpringCloudExampleApplication.java
+++ SpringCloudExample/src/main/java/com/example/SpringCloudExample/SpringCloudExampleApplication.java
@@ -2,8 +2,10 @@ package com.example.SpringCloudExample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudExampleApplication {
 
 	public static void main(String[] args) {
