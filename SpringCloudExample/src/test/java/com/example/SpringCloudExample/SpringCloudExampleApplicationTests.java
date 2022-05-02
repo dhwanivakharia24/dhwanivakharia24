dc8cfdef41f84731aa774cc73fbd19ec8daa6a39
Dhwani <dev1@a-enigma.com>
2022-05-02 20:57:06 +0530

    initial commit

---
+++ SpringCloudExample/src/test/java/com/example/SpringCloudExample/SpringCloudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringCloudExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
